f1d70447f6578e3a33ba2612daca7cb492e7c7ed
Jamesmalo <9jmalo@stpatrick.org>
2016-11-17 10:53:35 -0600

    started work

---
+++ pizza Malo.js
@@ -0,0 +1,32 @@
+class pizza{
+  //type constructor below
+  constructor(s,mt,vt){
+    this.size = s; //array for size [S,M,L,XL]
+    this.meatTopping = mt;
+    this.veggieTopping = vt;
+  }
+  //type instance functions below
+  sizeCost(){
+    if(this.size == "S"){
+
+    }
+    else if(this.size == "M"){
+
+    }
+    else if(this.size == "L"){
+
+    }
+    else if(this.size == "XL")
+  }
+  toppingCost(){
+
+  }
+  price(){
+
+  }
+  //type class functions below
+
+  static promotionalDeal(){
+
+  }
+}
